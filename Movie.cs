b8a88f8d7801b37e3e5d7fa4019fa902009be591
A253562 <A253562@LSBSA-9JJCSV3>
2023-12-07 01:33:52 +0200

    Add project files.

---
+++ Movie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettersandSetters
+{
+    internal class Movie
+    {
+        public string title;
+        public string director;
+        private string rating;
+
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating; // Using the property to set the rating
+        }
+
+        public string Rating
+        {
+            get { return rating; }
+            set
+            {
+                // Fixed the conditions and syntax
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettersandSetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie1 = new Movie("Titanic", "Jim", "PG-13");
+            Movie movie2 = new Movie("Unreleased", "Evans", "PG");
+
+            Console.WriteLine(movie1.Rating);
+            Console.ReadLine();
+        }
+    }
+}
